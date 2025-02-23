ac3cf0a103baa870c8ce0afdd01ea4d60d9677f7
Nicholas Pachulski <nick@pachulski.me>
2025-02-23 11:02:34 -0500

    Reformat lazygit.nvim

--- dotfiles/config/nvim/lua/plugins/lazygit.lua
+++ dotfiles/config/nvim/lua/plugins/lazygit.lua
@@ -1,7 +1,5 @@
 return {
 	"kdheepak/lazygit.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-	},
+	keys = { { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" } },
 }
