d57f896df7a7dec265da6bc1fd6b394b98888e95
Nicholas Pachulski <nick@pachulski.me>
2025-02-23 11:02:02 -0500

    Update lazygit config
    Lazy loaded plugins are not run until either one of their cmd's or key's
    are executed. We never use any of these cmd's; we only use the key
    shortcut, so we can remove them. Also, if either of the cmd or key table
    keys are set, lazy is implicitly true, so we can remove that table key.

--- dotfiles/config/nvim/lua/plugins/lazygit.lua
+++ dotfiles/config/nvim/lua/plugins/lazygit.lua
@@ -1,17 +1,7 @@
 return {
 	"kdheepak/lazygit.nvim",
-	lazy = true,
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
+	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
-		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit (Custom Config)" },
+		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
 	},
 }
