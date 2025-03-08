000569ef20252915956eb12bb8320f47278e493e
Nicholas Pachulski <nick@pachulski.me>
2025-03-08 13:14:43 -0500

    Quote test commands
    A lot of the test commands we run are for react native expo-router
    applications. There are file name conventions that require putting
    filenames inside square brackets, e.g.
    
    tests/screens/inbox/gmail_accounts/[gmailAccountId]/threads/[threadId].test.tsx
    
    Trying to run tests with paths that include those square brackets
    breaks unless those paths are quoted.
    
    This commit ensures that filename paths that are run are quoted.

--- dotfiles/config/nvim/lua/config/personal/vim_test.lua
+++ dotfiles/config/nvim/lua/config/personal/vim_test.lua
@@ -2,6 +2,22 @@ local helpers = require("config.personal.helpers")
 
 return {
 	setup = function()
+		vim.g["test#custom_transformations"] = {
+			quote_paths = function(cmd)
+				if type(cmd) == "string" then
+					return cmd:gsub("(%S+%.test%.tsx)", '"%1"')
+				elseif type(cmd) == "table" then
+					for i, part in ipairs(cmd) do
+						cmd[i] = part:gsub("(%S+%.test%.tsx)", '"%1"')
+					end
+					return cmd
+				end
+				return cmd
+			end,
+		}
+
+		vim.g["test#transformation"] = "quote_paths"
+
 		vim.g["test#custom_strategies"] = {
 			send_to_tmux_pane = function(cmd)
 				if type(cmd) == "table" then
